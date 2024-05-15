5bba38272993945b2f470c52c745a03118dbfe02
Saydenier Palacios <saydenier@gmail.com>
2024-05-15 00:56:57 -0400

    Publication fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Tkr0n.github.io',
+  base: 'Portfolio',
+});
